c41fb3559ee1a20f52e808a98d8460e181e5a0f8
InesKamFou <134599842+InesKamFou@users.noreply.github.com>
2023-06-01 09:35:22 +0100

    change

--- FirstPythonCode.py
+++ FirstPythonCode.py
@@ -1,1 +1,7 @@
-print("hello Python")
+#create a list of strings
+listOfStrings = ['one', 'two', 'three', 'four', 'five']
+#iterate over the list
+
+for item in listOfStrings:
+    print(item)
+    
